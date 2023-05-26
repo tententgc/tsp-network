bf2ab608d4a33c877e8ccb6845649b0f00478b5c
Thanyapisit Buaprakong <nyman3527@gmail.com>
2023-05-26 15:31:48 +0700

    push connection with distance

--- source/test1/index.js
+++ source/test1/index.js
@@ -9,25 +9,6 @@ const readline = require("readline").createInterface({
 // Parse peer's IP address list
 const name = process.argv[2];
 const peers = process.argv[3].split(",");
-const distances = {
-	"node1": { "node2": 3, "node3": 5, "node4": 7 },
-	"node2": { "node1": 3, "node3": 2, "node4": 6 },
-	"node3": { "node1": 5, "node2": 2, "node4": 4 },
-	"node4": { "node1": 7, "node2": 6, "node3": 4 },
-};
-
-function calculateDistance(...nodes) {
-	let totalDistance = 0;
-
-	for (let i = 0; i < nodes.length - 1; i++) {
-		const currentNode = nodes[i];
-		const nextNode = nodes[i + 1];
-		console.log("currentNode : ", currentNode,"distance : ", distances[currentNode][nextNode], "nextNode : ", nextNode)
-		totalDistance += distances[currentNode][nextNode];
-	}
-
-	return totalDistance;
-}
 
 // Create socket server (for retrieving broadcast)
 var server = net.createServer((socket) => {
@@ -43,10 +24,6 @@ var server = net.createServer((socket) => {
 		// Log payload information
 		console.log(remoteTag + "Recieved payload: " + payload);
 
-		const distance_node = payload.split(", ")
-		console.log("distance_node : ", distance_node)
-		const distance = calculateDistance(...distance_node) 
-		console.log("distance : ", distance)
 		// Check if payload already passed through current node
 		if (payload.split(", ").includes(name)) {
 			console.log(remoteTag + "Repeated routing, discarded!");
@@ -61,11 +38,11 @@ var server = net.createServer((socket) => {
 		peers.forEach((peer) => {
 			const client = new net.Socket();
 			client.connect(1337, peer, () => {
-				// console.log(remoteTag + "Connected to " + peer);
+				console.log(remoteTag + "Connected to " + peer);
 				client.write(appended);
-				// console.log(remoteTag + "Writing with " + appended);
+				console.log(remoteTag + "Writing with " + appended);
 				client._destroy(null, () => {
-					// console.log(remoteTag + "Disconnected from " + peer);
+					console.log(remoteTag + "Disconnected from " + peer);
 				});
 			});
 		});
@@ -80,11 +57,11 @@ const prompt = async () => {
 		}
 		const client = new net.Socket();
 		client.connect(1337, peer, () => {
-			// console.log("[CLIENT] Connected to " + peer);
+			console.log("[CLIENT] Connected to " + peer);
 			client.write(name);
-			// console.log("[CLIENT] Writing with " + name);
+			console.log("[CLIENT] Writing with " + name);
 			client._destroy(null, () => {
-				// console.log("[CLIENT] Disconnected from " + peer);
+				console.log("[CLIENT] Disconnected from " + peer);
 			});
 		});
 		prompt();
